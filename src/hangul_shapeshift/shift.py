ef6b11d9fc11694f941d45eaa81e62c60d862d8e
ne0ekspert <rlaaudwk5503@gmail.com>
2024-05-15 11:36:28 +0900

    function documentation

--- src/hangul_shapeshift/shift.py
+++ src/hangul_shapeshift/shift.py
@@ -3,20 +3,32 @@ from jamo import j2hcj, h2j, j2h
 import math
 
 def shift(source: str, target: str) -> list[str]:
+    """
+    Args:
+        source (str): A character of Hangul
+        target (str): A character of Hangul
+    
+    Returns:
+        list[str]: Steps of transition, each items has one character.
+    
+    Example:
+        >>> shift('각', '닳')
+        ['감', '갊', '낢', '날', '달', '닳']
+    """
     assert len(source) == 1
     assert len(target) == 1
 
-    source = list(j2hcj(h2j(source)))
-    target = list(j2hcj(h2j(target)))
+    s = list(j2hcj(h2j(source)))
+    t = list(j2hcj(h2j(target)))
 
-    while len(source) != 3:
-        source.append(' ')
-    while len(target) != 3:
-        source.append(' ')
+    while len(s) != 3:
+        s.append(' ')
+    while len(t) != 3:
+        s.append(' ')
 
-    cho_path = CHO_NX.shortest_path(source[0], target[0])
-    jung_path = JUNG_NX.shortest_path(source[1], target[1])
-    jong_path = JONG_NX.shortest_path(source[2], target[2])
+    cho_path = CHO_NX.shortest_path(s[0], t[0])
+    jung_path = JUNG_NX.shortest_path(s[1], t[1])
+    jong_path = JONG_NX.shortest_path(s[2], t[2])
 
     lcm = math.lcm(len(cho_path), len(jung_path), len(jong_path))
 
